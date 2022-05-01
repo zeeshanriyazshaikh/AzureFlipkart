14655ee687becd041e1716abfd670fc2f2c0d60c
unknown <danishmansurii1993@gmail.com>
2022-05-01 16:53:00 +0530

    azureflipkart

---
+++ src/test/java/AzureTestPackage/AzureTest.java
@@ -0,0 +1,37 @@
+package AzureTestPackage;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class AzureTest {
+	ChromeDriver driver;
+	@BeforeMethod
+	public void setUp() {
+		System.setProperty("webdriver.chrome.driver","C:\\\\Users\\\\Faiz Riyaz\\\\Desktop\\\\chrome driver\\\\chromedriver.exe");
+	driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().deleteAllCookies();
+		driver.manage().timeouts().pageLoadTimeout(40,TimeUnit.SECONDS);
+		driver.manage().timeouts().implicitlyWait(40,TimeUnit.SECONDS);
+		driver.get("https://www.flipkart.com/");
+	}
+	
+	@Test                                 //1.TITLE TEST
+	public void sidraTitleTest() {
+	String title = driver.getTitle();
+	System.out.println(title); 
+	Assert.assertEquals(title, "Online Shopping Site for Mobiles, Electronics, Furniture, Grocery, Lifestyle, Books & More. Best Offers!","title not match"); 
+	}
+
+
+	@AfterMethod
+	public void tearDown() {
+		driver.quit();
+	}
+}
